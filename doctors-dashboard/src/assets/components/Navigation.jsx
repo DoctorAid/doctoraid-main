99729ea09b7128df57ccc4c36c8f143709fa9813
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-16 13:07:34 +0530

    navigation background colour added

--- doctors-dashboard/src/assets/components/Navigation.jsx
+++ doctors-dashboard/src/assets/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navigation() {
   return (
     <div>
-        <div className="w-[266px] h-[1078px] bg-[#e1e1e2] rounded-[30px]">
+        <div className="w-[266px] h-[1078px] bg-[#295567] rounded-[30px]">
 
 
             ssssssssssssssssss
