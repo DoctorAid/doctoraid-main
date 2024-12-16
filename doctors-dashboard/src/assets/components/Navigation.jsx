c83dbaeb4006ca0efd033a949e1ef2eb675c38bf
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-16 13:02:14 +0530

    stylings updated

--- doctors-dashboard/src/assets/components/Navigation.jsx
+++ doctors-dashboard/src/assets/components/Navigation.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Navigation() {
   return (
     <div>
-        <p>Hello000000000000000000000000000</p>
+        <div className="w-[266px] h-[1078px] bg-[#e1e1e2] rounded-[30px]">
+
+
+            ssssssssssssssssss
+        </div>
+       
       
     </div>
   )
