a9dd49344fa68e094e4e7a90e6effc78f470d029
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-17 08:06:06 +0530

    logo added to navigation

--- doctors-dashboard/src/assets/components/Navigation.jsx
+++ doctors-dashboard/src/assets/components/Navigation.jsx
@@ -1,12 +1,19 @@
+
 import React from 'react'
 
 function Navigation() {
   return (
     <div>
-        <div className="w-[266px] h-[1078px] bg-[#295567] rounded-[30px]">
+        <div className="flex flex-col w-[266px] h-[1078px] py-8 bg-[#295567] rounded-[30px]">
+          <div className='flex flex-col items-center justify-center'>
+            <div className="flex items-center absolute">
+            <img src="/Images/logo.png" className="w-full h-full object-cover"/>
+        </div>
+           
 
+          </div>
 
-            ssssssssssssssssss
+        
         </div>
        
       
