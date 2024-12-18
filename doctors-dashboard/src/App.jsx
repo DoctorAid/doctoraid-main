4a20c6449d7d777262bee132b06e3255e8bbf200
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-18 15:59:37 +0530

    dashboard navigation added

--- doctors-dashboard/src/App.jsx
+++ doctors-dashboard/src/App.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
-import Navigation from './assets/components/Navigation'
+import DashboardNavigation from './assets/page_sections/dashboard/DashboardNavigation'
 
 function App() {
   return (
     <div className='flex flex-col gap-5 bg-[#FAFAF9] w-full  px-2 py-2 items-start justify-start text-black'>
       
-      <div className='flex justify-between w-full h-[10%] bg-[#93b1ec] items-center px-1 py-1'>
-        <div className='bg-[#ed7e7e] w-full h-full'><p>ssssssss</p></div>
-        <div className='bg-[#f72626]'><p>ssssssss</p></div>
-        <p>ssssssss</p>
-      </div>
+      <DashboardNavigation/>
 
-      <div className='flex justify-around w-full h-[40%] bg-[#93b1ec] items-center'>
-        <div className='bg-[#b7f877]'><p>Section 01</p></div>
-        <div className='bg-[#3bcf3b]'><p>Section 02</p></div>
+      <div className='flex justify-around gap-2 w-full h-[40%] bg-[#93b1ec] items-center px-1 py-1'>
+        <div className='bg-[#b7f877] w-full h-full'><p>Section 01</p></div>
+        <div className='bg-[#3bcf3b] w-full h-full'><p>Section 02</p></div>
        
       </div>
 
-      <div className='flex justify-around w-full h-[60%] bg-[#93b1ec] items-center'>
-        <div className='bg-[#FFFF]'><p>Section 01</p></div>
-        <div><p>Section 02</p></div>
-        <div><p>Section 03</p></div>
+      <div className='flex justify-around gap-2 w-full h-[60%] bg-[#93b1ec] items-center px-1 py-1'>
+        <div className='bg-[#2e38f8] w-full h-full'><p>Section 01</p></div>
+        <div className='bg-[#7da4f2] w-full h-full'><p>Section 02</p></div>
+        <div className='bg-[#2761a8] w-full h-full'><p>Section 03</p></div>
        
       </div>
       
---
+++ doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function DashboardNavigation() {
+  return (
+    <div className='flex justify-between w-full h-[10%] gap-2 bg-[#93b1ec] items-center px-1 py-1'>
+        <div className='bg-[#ed7e7e] w-full h-full'><p>Section 01</p></div>
+        <div className='bg-[#f72626] w-full h-full'><p>Section 02</p></div>
+        <div className='bg-[#643a3a] w-full h-full'><p>Section 03</p></div>
+        
+      </div>
+  )
+}
+
+export default DashboardNavigation
