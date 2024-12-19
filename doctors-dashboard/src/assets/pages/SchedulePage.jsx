70c2617e8b870ce23ba375eea7b29794859841ce
Sakith-Seneviratne <Sakithsene@gmail.com>
2024-12-19 10:50:54 +0530

    Added Sections to Schedule Page

--- doctors-dashboard/src/assets/pages/SchedulePage.jsx
+++ doctors-dashboard/src/assets/pages/SchedulePage.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 function SchedulePage() {
   return (
-    <div className='flex bg-white w-full items-center justify-center text-black'>
-
-
-       <div>
-       <img src="/Images/temp.png" className="w-40 h-64" />Information about sessions</div> 
+    <div className='flex flex-col gap-5 bg-[#FAFAF9] w-full   px-2 py-2 items-start justify-start text-black'>
       
+    <div className='flex justify-around gap-2 w-full h-[100%] bg-[#93b1ec] items-center px-1 py-1'>
+      <div className='bg-[#b7f877] w-full h-full'><p>Section 01</p>
+      <div className='bg-[#C9E4F3] w-full h-full'><p>Section 01</p></div>
+      </div>
+      
+      <div className='bg-[#3bcf3b] w-full h-full'><p>Section 02</p></div>
+     
     </div>
+    
+    
+  </div>
   )
 }
 
