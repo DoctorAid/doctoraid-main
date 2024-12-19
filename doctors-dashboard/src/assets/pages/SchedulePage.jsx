78debf5bf85e4cb3af5ed974a8da43e01b08ef8a
Sakith-Seneviratne <Sakithsene@gmail.com>
2024-12-19 11:37:02 +0530

    adding paddings to Schedule section

--- doctors-dashboard/src/assets/pages/SchedulePage.jsx
+++ doctors-dashboard/src/assets/pages/SchedulePage.jsx
@@ -2,19 +2,30 @@ import React from 'react'
 
 function SchedulePage() {
   return (
-    <div className='flex flex-col gap-5 bg-[#FAFAF9] w-full   px-2 py-2 items-start justify-start text-black'>
-      
+    <div className='flex flex-col gap-5 bg-[#FAFAF9] w-full  px-2 py-2 items-start justify-start text-black'>
     <div className='flex justify-around gap-2 w-full h-[100%] bg-[#93b1ec] items-center px-1 py-1'>
-      <div className='bg-[#b7f877] w-full h-full'><p>Section 01</p>
-      <div className='bg-[#C9E4F3] w-full h-full'><p>Section 01</p></div>
-      </div>
-      
-      <div className='bg-[#3bcf3b] w-full h-full'><p>Section 02</p></div>
+      <div className='bg-[#b7f877] w-full h-full'><p>Section 01</p></div>
+      <div className='flex flex-col gap-2 bg-[#3gcf3b] w-full h-full'>
+        <div className='flex bg-[#3bff3b] w-full h-[50%]'>
+        <p>Section 02</p>
+        </div>
+        <div className='flex bg-[#3bcf3b] w-full h-[50%]'>
+        <p>Section 02</p>
+        </div>
+        
+        
+        
+        </div>
      
     </div>
+
+   
     
     
   </div>
+    
+
+
   )
 }
 
