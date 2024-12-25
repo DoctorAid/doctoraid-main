606585f39ed25f3daa6183a07de550bc26ad7feb
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-25 09:23:44 +0530

    sections devided

--- doctors-dashboard/src/App.jsx
+++ doctors-dashboard/src/App.jsx
@@ -8,15 +8,15 @@ function App() {
       <DashboardNavigation/>
 
       <div className='flex justify-around gap-2 w-full h-[40%] bg-[#93b1ec] items-center px-1 py-1'>
-        <div className='bg-[#b7f877] w-full h-full'><p>Section 01</p></div>
-        <div className='bg-[#3bcf3b] w-full h-full'><p>Section 02</p></div>
+        <div className='bg-[#b7f877] w-full h-full flex items-center justify-center'><p>Section 01</p></div>
+        <div className='bg-[#3bcf3b] w-full h-full flex items-center justify-center'><p>Section 02</p></div>
        
       </div>
 
       <div className='flex justify-around gap-2 w-full h-[60%] bg-[#93b1ec] items-center px-1 py-1'>
-        <div className='bg-[#2e38f8] w-full h-full'><p>Section 01</p></div>
-        <div className='bg-[#7da4f2] w-full h-full'><p>Section 02</p></div>
-        <div className='bg-[#2761a8] w-full h-full'><p>Section 03</p></div>
+        <div className='bg-[#2e38f8] w-full h-full flex items-center justify-center'><p>Section 01</p></div>
+        <div className='bg-[#7da4f2] w-full h-full flex items-center justify-center'><p>Section 02</p></div>
+        <div className='bg-[#2761a8] w-full h-full flex items-center justify-center'><p>Section 03</p></div>
        
       </div>
       
--- doctors-dashboard/src/assets/pages/SchedulePage.jsx
+++ doctors-dashboard/src/assets/pages/SchedulePage.jsx
@@ -3,8 +3,23 @@ import React from 'react'
 function SchedulePage() {
   return (
     <div className='flex flex-col gap-5 bg-[#FAFAF9] w-full  px-2 py-2 items-start justify-start text-black'>
-    <div className='flex justify-around gap-2 w-full h-[100%] bg-[#93b1ec] items-center px-1 py-1'>
-      <div className='bg-[#b7f877] w-full h-full'><p>Section 01</p></div>
+    <div className='flex justify-around gap-2 w-full h-[100%]  items-center px-1 py-1'>
+
+      {/* left side */}
+      <div className=' flex flex-col sm:flex-wrap justify-between w-full h-full'>
+        <div className=' w-full h-[10%]'>
+        <div className="text-black text-[26px] font-bold font-['Raleway'] leading-normal">Current Session</div>
+        </div>
+        <div className='bg-[#e531d6] w-full h-[50%]'>
+          <p>Section 01</p>
+        </div>
+        <div className='bg-[#d22632] w-full h-[30%]'>
+          <p>Section 01</p>
+        </div>
+        
+      </div>
+
+        {/* right side */}
       <div className='flex flex-col gap-2 bg-[#3gcf3b] w-full h-full'>
         <div className='flex bg-[#3bff3b] w-full h-[50%]'>
         <p>Section 02</p>
