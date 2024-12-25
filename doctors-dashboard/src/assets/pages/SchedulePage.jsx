4a3e789ec678bef0a5fe054f4f9d370437b80e56
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2024-12-25 10:07:18 +0530

    header elements updated

--- doctors-dashboard/src/assets/pages/SchedulePage.jsx
+++ doctors-dashboard/src/assets/pages/SchedulePage.jsx
@@ -7,9 +7,42 @@ function SchedulePage() {
 
       {/* left side */}
       <div className=' flex flex-col sm:flex-wrap justify-between w-full h-full'>
+        {/* header */}
         <div className=' w-full h-[10%]'>
-        <div className="text-black text-[26px] font-bold font-['Raleway'] leading-normal">Current Session</div>
+
+          {/* header 01  */}
+          <div className="text-black text-[26px] font-bold font-['Raleway'] leading-normal">Current Session</div>
+          {/* header 02  */}
+          <div className='flex justify-between'>
+
+            {/* header date */}
+            <div className="flex items-center w-[88px] h-[88px] bg-[#295567] rounded-[10px]">
+              <div className="w-[84px] h-[60px] text-center"><span class="text-[#fefaf6] text-[32px] font-bold font-['Raleway'] leading-7">
+                20<br/></span>
+                <span class="text-[#fefaf6] text-[32px] font-normal font-['Raleway'] leading-7">Sun</span>
+              </div>
+            </div>
+
+            <div className='flex flex-col justify-center'>
+              <div><span class="text-[#152945] text-2xl font-bold font-['Raleway']">Totol Slots</span>
+                  <span class="text-black text-2xl font-medium font-['Raleway'] leading-normal"> - </span>
+                  <span class="text-black text-2xl font-normal font-['Raleway'] leading-normal">172</span>
+              </div>
+              <div className="w-[181px] items-center justify-center h-10 bg-[#cdffcd] rounded-[10px]">
+                <div><span class="text-[#295567] text-2xl font-bold font-['Raleway'] leading-normal">Booked - </span>
+                      <span class="text-[#295567] text-2xl font-normal font-['Raleway'] leading-normal">20</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+
+          <div className=''></div>
+
+          <div className=''></div>
         </div>
+
         <div className='bg-[#e531d6] w-full h-[50%]'>
           <p>Section 01</p>
         </div>
