9f9b2c89eb34c77bd7ddae92bf7125b7bbb46a15
thanushiii <thanushi.20230930@iit.ac.lk>
2024-12-29 17:12:31 +0530

    dashboard section 1

--- doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
+++ doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-function DashboardNavigation() {
-  return (
-    <div className='flex justify-between w-full h-[10%] gap-2 bg-[#93b1ec] items-center px-1 py-1'>
-        <div className='bg-[#ed7e7e] w-full h-full flex items-center justify-center'><p>Section 01</p></div>
-        <div className='bg-[#f72626] w-full h-full flex items-center justify-center'><p>Section 02</p></div>
-        <div className='bg-[#643a3a] w-full h-full flex items-center justify-center'><p>Section 03</p></div>
-        
+function DashboardNavigation(){
+  return(
+    <div className='flex justify-between w-full h-[10%] gap-2 bg bg-white items-center px-1 py-1'>
+      <div className='w-full h-full flex flex-col items-start justify-center px-4'>
+        <p className="text-[#1a237e] font-bold">Hi, <span className="text-[#82b1ff]">Dr.Bessie</span></p>
       </div>
-  )
+
+      <div className='bg-[#f72626] w-full h-full flex items-center justify-center'>
+        <p className="text-[#1a237e] font-bold">Section 02</p>
+      </div>
+
+      <div className='bg-[#643a3a] w-full h-full flex items-center justify-center'>
+        <p className="text-[#1a237e] font-bold">Section 03</p>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardNavigation
