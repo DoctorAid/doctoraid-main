31a687a1e093a290075e29f197d5bda6561894b6
thanushiii <thanushi.20230930@iit.ac.lk>
2024-12-29 20:08:00 +0530

    Update DashboardNavigation.jsx

--- doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
+++ doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 function DashboardNavigation(){
   return(
@@ -8,7 +9,17 @@ function DashboardNavigation(){
         <p className="text-[#1a237e] font-bold">Today is Monday, 20th November 2024</p>
       </div>
 
-      <div className='bg-[#f72626] w-full h-full flex items-center justify-center'><p>Section 02</p></div>
+      <div className='w-full h-full flex items-center justify-center px-4'>
+        <div className="flex items-center w-full h-3/4 bg-[#82b1ff] rounded-lg px-4">
+          <FaSearch className="text-white text-sm mr-2"/>
+          <input type="text" className="w-full bg-transparent text-white text-sm focus:outline-none placeholder-white" placeholder="Search a patient"></input>
+          <div className="flex flex-col justify-center items-end gap-[2px]">
+            <div className="w-6 h-[2px] bg-white"></div>
+            <div className="w-4 h-[2px] bg-white"></div>
+            <div className="w-2 h-[2px] bg-white"></div>
+          </div>
+        </div>
+      </div>
 
       <div className='bg-[#643a3a] w-full h-full flex items-center justify-center'><p>Section 03</p></div>
     </div>
