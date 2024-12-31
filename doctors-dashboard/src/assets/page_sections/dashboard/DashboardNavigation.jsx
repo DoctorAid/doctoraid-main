b2323dae3e9a0a011e6d69c31b4028d7b5cb3f08
LakmindeeJayamanne <Lakmindee2001@gmail.com>
2024-12-31 11:18:21 +0530

    adjusting the navigation bar of doctors dashboard

--- doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
+++ doctors-dashboard/src/assets/page_sections/dashboard/DashboardNavigation.jsx
@@ -4,13 +4,13 @@ import { FaSearch } from 'react-icons/fa'; // Importing FontAwesome search icon
 
 function DashboardNavigation() {
   return (
-    <div className='flex justify-between w-full h-[10%] gap-2 bg-white items-center px-1 py-1'>
-        <div className='w-full h-full flex flex-col items-start justify-center px-4'>
+    <div className='flex justify-between w-full h-[10%] gap-2 bg-white items-center px-6 py-3'>
+        <div className='flex flex-col justify-center'>
           <p className="text-[#1a237e] font-bold">Hi, <span className="text-[#82b1ff]">Dr. Bessie</span></p>
           <p className="text-[#1a237e] font-bold">Today is Monday, 20th November 2024</p>
         </div>
-        <div className='w-full h-full flex items-center justify-center px-4'>
-          <div className="flex items-center w-full h-3/4 bg-[#82b1ff] rounded-lg px-4">
+        <div className='flex-grow flex items-center justify-center mx-6'>
+          <div className="flex items-center w-full max-w-md h-10 bg-[#82b1ff] rounded-lg px-4 shadow-md">
             <div className="flex flex-col justify-center items-end gap-[2px]">
               <div className="w-5 h-[2px] bg-black"></div>
               <div className="w-5 h-[2px] bg-black"></div>
