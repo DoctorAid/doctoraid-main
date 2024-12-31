cdff7cf3387b19f6df4bb1f602410d06a53ddca6
thanushiii <thanushi.20230930@iit.ac.lk>
2024-12-31 22:19:01 +0530

    Update PatientsPage.jsx

--- doctors-dashboard/src/assets/pages/PatientsPage.jsx
+++ doctors-dashboard/src/assets/pages/PatientsPage.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function PatientsPage() {
   return (
-    <div className='flex bg-[#FAFAF9]  w-full items-center justify-center text-black'>
+    <div className="flex bg-[#FAFAF9] w-full h-screen items-start justify-between text-black p-6 gap-4">
+      {/* Placeholder for Session's Patient List */}
+      <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
+        <p>Session's List</p>
+      </div>
 
+      {/* Placeholder for On-going Section */}
+      <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
+        <p>On-going</p>
+      </div>
 
-       <div>
-       <img src="/Images/temp.png" className="w-40 h-64" />Information about patients</div> 
-      
+      {/* Consultation History Section */}
+      <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
+        <p>Consultation History</p>
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default PatientsPage
+export default PatientsPage;
