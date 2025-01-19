16c36ec826cb3479b2ee192093e978adbedfe5fc
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2025-01-19 17:43:23 +0530

    loads the dashboard after sign in

--- doctors-dashboard/src/assets/pages/Auth/SignInPage.jsx
+++ doctors-dashboard/src/assets/pages/Auth/SignInPage.jsx
@@ -1,10 +1,22 @@
 import { SignIn } from "@clerk/clerk-react";
+import { useAuth } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function SignInPage() {
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      useNavigate("/dashboard");
+    }
+  }, [isSignedIn, navigate]);
+
   return (
-    <main className="flex items-center justify-center min-h-screen container px-4">
+    <div className="flex justify-center items-center h-screen">
       <SignIn />
-    </main>
+    </div>
   );
 }
 
