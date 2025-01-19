64f99a024deba930bf5f0bb692c0208cf6a29e29
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2025-01-19 16:41:02 +0530

    publishable key imported

--- doctors-dashboard/src/main.jsx
+++ doctors-dashboard/src/main.jsx
@@ -11,6 +11,12 @@ import PatientsPage from './assets/pages/PatientsPage.jsx'
 import ErrorPage from './assets/pages/ErrorPage.jsx'
 
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 const router = createBrowserRouter(
   [{
     element:<RootLayout/>,
