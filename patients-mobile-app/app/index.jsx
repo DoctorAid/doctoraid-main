16b15022d3fcf944a7223931ee9c32607008e552
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2025-02-03 19:12:40 +0530

    Navigation tab pages added

--- patients-mobile-app/app/index.jsx
+++ patients-mobile-app/app/index.jsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const StartPage = () => {
+ 
+    return <Redirect href="/home" />;
+  
 }
+
+export default StartPage;
