dc401bd803e23d7a16991dfde85dc2118dc80473
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2025-02-03 23:40:05 +0530

    Splash screen added

--- patients-mobile-app/app/index.jsx
+++ patients-mobile-app/app/index.jsx
@@ -3,9 +3,47 @@ import { Redirect } from "expo-router";
 import { useEffect, useState } from "react";
 
 const StartPage = () => {
- 
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    // 3-second delay before redirecting
+    const timer = setTimeout(() => {
+      setIsReady(true); // isReady will be to true after 3 seconds
+    }, 3000);
+
+    // Clear the timeout if the component is unmounted before 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isReady) {
     return <Redirect href="/home" />;
-  
-}
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./Assets/images/Logo.png')} 
+        style={styles.logo}
+      />  
+        
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#C9E4F3', 
+  },
+  logo: {
+    width: 83, 
+    height: 95,
+    resizeMode: 'contain', 
+    marginBottom: 20, 
+  },
+});
 
 export default StartPage;
