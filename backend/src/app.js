448c83a0b65003bcbedbfe288c780c64e28a8be6
Nimesha Savinda Dahanayaka <159873731+nimesha-savinda@users.noreply.github.com>
2025-02-07 08:34:14 +0530

    refactor backend setup: switch to ES modules, update main entry point, and add database connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express from 'express';
+import mongoose from 'mongoose';
+import { connectDB } from './infrastructure/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('DoctorAid API Running'));
 
-app.listen(PORT, () => console.log(`Serkkkrunning on http://localhost:${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+connectDB();
