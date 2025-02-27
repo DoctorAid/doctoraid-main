e6e4f7b4fd072e0fe9559151efc431f1bc3b0ee6
LakmindeeJayamanne <Lakmindee2001@gmail.com>
2025-02-27 13:33:26 +0530

    error fixed in patient api

--- backend/src/dashboard/api/patientAPI.js
+++ backend/src/dashboard/api/patientAPI.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
-import {searchPatients} from '..application/dto/patient.js';
-import {getPatientList} from '..application/dto/patient.js';
+import {searchPatients} from '../application/dto/patient.js';
+import {getPatientList} from '../application/dto/patient.js';
 
 
 const patientRouter = express.Router();
 
-patientRouter.get('search/byName', searchPatients); //search patients by their name
-patientRouter.get('search/byEmail', searchPatients);    //search patients by thier email
+patientRouter.get('/search/byName', searchPatients); //search patients by their name
+patientRouter.get('/search/byEmail', searchPatients);    //search patients by thier email
 patientRouter.get('getPatients', getPatientList);   //get the patient list
 
 
