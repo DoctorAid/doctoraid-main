29199c8e418c19c35695ddd905ea384fbb40e018
Miyunika07 <cmiyunika@gmail.com>
2025-03-01 09:52:56 +0530

    add endpoint to fetch patients list by doctor ID and session date, the patients list

--- backend/src/dashboard/api/sessionsAPI.js
+++ backend/src/dashboard/api/sessionsAPI.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import { getSessions } from '../application/dto/sessions.js';
+import { getPatientsList } from '../application/dto/sessions.js';
 
 const sessionsRouter = express.Router();
 
 sessionsRouter.get('/get', getSessions);
+sessionsRouter.get('/patientslist', getPatientsList);
 
 export default sessionsRouter;
