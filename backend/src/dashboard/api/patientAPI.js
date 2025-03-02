9fda2ae30976572f959741983d5fce392d0575de
LakmindeeJayamanne <Lakmindee2001@gmail.com>
2025-03-02 21:12:20 +0530

    creating endpoint to get the patient profile by patient Id

--- backend/src/dashboard/api/patientAPI.js
+++ backend/src/dashboard/api/patientAPI.js
@@ -3,6 +3,7 @@ import express from 'express';
 import {searchPatients} from '../application/dto/patient.js';
 import {getPatientList} from '../application/dto/patient.js';
 import {sortPatientList} from '../application/dto/patient.js';
+import {getPatientProfile} from '../application/dto/patient.js';
 
 const patientRouter = express.Router();
 
@@ -10,6 +11,7 @@ patientRouter.get('/search/byName', searchPatients); //search patients by their 
 patientRouter.get('/search/byEmail', searchPatients);    //search patients by thier email
 patientRouter.get('/getPatients', getPatientList);   //get the patient list
 patientRouter.get('/sort/byName', sortPatientList);    //sorting the patient list in ascending/ decending order
+patientRouter.get('/getPatientProfile/byPatientId', getPatientProfile);  //retrieving patient profile by the patient id
 
 
 export default patientRouter;
