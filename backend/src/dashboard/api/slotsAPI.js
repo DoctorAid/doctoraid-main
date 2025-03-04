a9dbc914e63bcee93fafafe7a7a8b743e14d038b
LakmindeeJayamanne <Lakmindee2001@gmail.com>
2025-03-04 12:03:40 +0530

    added cancelling appointment function

--- backend/src/dashboard/api/slotsAPI.js
+++ backend/src/dashboard/api/slotsAPI.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { getSlotsbySession } from '../application/dto/slots.js';
 import { createSlots } from "../application/dto/slots.js";
-import { cancelAppoinment } from "../application/dto/slots.js";
+import { cancelAppointment } from "../application/dto/slots.js";
 
 const slotsRouter = express.Router();
 
 slotsRouter.post('/create', createSlots);
 slotsRouter.get('/getbySessionId', getSlotsbySession);
-slotsRouter.put('/cancel/bySessionId', cancelAppoinment);
+slotsRouter.put('/cancel/bySessionId', cancelAppointment);
 
 export default slotsRouter;
