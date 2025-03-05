f20da8e61150746de4bada115fc1d73c2736d8ae
Sandith02 <lhthenuwara@gmail.com>
2025-03-05 13:07:12 +0530

    Updated MongoDB connection string to DoctorAid db

--- backend/src/infrastructure/db.js
+++ backend/src/infrastructure/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
     try{
-        const connectionstring = "mongodb+srv://doctoraidhealth:13mm9HP2taCpPtha@doctoraidcluster01.wxts6.mongodb.net/?retryWrites=true&w=majority&appName=DoctorAidCluster01";
+        const connectionstring = "mongodb+srv://doctoraidhealth:13mm9HP2taCpPtha@doctoraidcluster01.wxts6.mongodb.net/DoctorAid?retryWrites=true&w=majority&appName=DoctorAidCluster01";
         await mongoose.connect(connectionstring);
         console.log("Conected to the database...!");
     }catch(error){
