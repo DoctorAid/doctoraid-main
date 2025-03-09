0e32263349fec92768dc127ea297ce8c4fd288a8
Sandith02 <lhthenuwara@gmail.com>
2025-03-09 19:15:55 +0530

    fix: correct endpoint path for fetching sessions by doctor

--- backend/src/dashboard/api/doctorAPI.js
+++ backend/src/dashboard/api/doctorAPI.js
@@ -5,7 +5,7 @@ const doctorRouter = express.Router();
 
 doctorRouter.get('/details', getDoctorDetails); //get the doctor details
 doctorRouter.post('/create', createDoctor ); //create a doctor profile
-doctorRouter.get('sessions', getSessionsByDoctor); //get the sessions based on doctorAid
+doctorRouter.get('/sessions', getSessionsByDoctor); //get the sessions based on doctorAid
 doctorRouter.get('/patients', getPatientsByDoctor); //get the patients based on doctorId
 doctorRouter.post('/addPatient', addDoctorToPatient); //add a patient to the doctor
 doctorRouter.get('/activeAppointments', getActiveAppointmentsByDoctor); //get the active appointments based on doctorId
