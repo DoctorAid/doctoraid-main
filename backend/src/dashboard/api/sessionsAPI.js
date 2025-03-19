7ab4a8860cd75722b435dae14656f285b9f93081
Sandith02 <lhthenuwara@gmail.com>
2025-03-19 08:57:30 +0530

    refactor: update sessions API routes to include session ID for waiting list and patient list retrieval

--- backend/src/dashboard/api/sessionsAPI.js
+++ backend/src/dashboard/api/sessionsAPI.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getSessions } from '../application/dto/sessions.js';
-import { getWaitingList, getPatientList } from '../application/dto/sessions.js';
+
+import { 
+    getSessions,
+    getWaitingList,
+    getPatientList 
+} from '../application/dto/sessions.js';
 
 const sessionsRouter = express.Router();
 
 sessionsRouter.get('/get', getSessions);
-sessionsRouter.get('/waitingList', getWaitingList);
-sessionsRouter.get('/patientList', getPatientList);
+sessionsRouter.get('/:sessionId/waitingList', getWaitingList);
+sessionsRouter.get('/:sessionId/patientList', getPatientList);
+
 export default sessionsRouter;
